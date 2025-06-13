629afbee178ea58b8ac6e551b31d240a901dfb9c
luis <luischoqueacostupa@gmail.com>
2025-06-13 18:21:04 -0500

    Map y RequestMapping

--- src/main/java/com/proyectox/demo/controllers/RestControllerEjemplo.java
+++ src/main/java/com/proyectox/demo/controllers/RestControllerEjemplo.java
@@ -1,13 +1,22 @@
 package com.proyectox.demo.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class RestControllerEjemplo {
-    @GetMapping("/api")
-    public String getApi(){
-        return "hola";
+    @GetMapping(path = "/info")
+    public Map<String, Object> getApi(){
+        Map<String, Object> respuesta = new HashMap<>();
+        respuesta.put("titulo", "Aprendiendo Spring Boot");
+        respuesta.put("Servidor","infoConfig");
+        respuesta.put("ip", "192.168.1.1");
+        return respuesta;
     }
 }
